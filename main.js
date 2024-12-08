977b2b57bd553ed42324660e68951442f652a5cd
achen443 <achen443@gatech.edu>
2024-12-07 20:18:13 -0500

    Styling hack

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 import { stateToRegion, regionColors } from './stateToRegion.js';
 
+const scatterContainer = d3.select("#scatterplot")
+    .append("div")
+    .attr("id", "scatter-container")
+    .style("display", "inline-block")
+    .style("vertical-align", "top");
+    
 // Set dimensions for the scatterplot
 const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 // Append an SVG element to the body
-const svg = d3
-    .select("body")
+const svg = scatterContainer
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
